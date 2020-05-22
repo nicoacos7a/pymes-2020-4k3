7c07a861ff25aad588a0ca2ce6e7f93890898b9e
nicoacos7a <nicoacos7a@gmail.com>
2020-05-22 19:58:26 -0300

    1 file modified

---
+++ src/app/models/articulo.ts
@@ -0,0 +1,10 @@
+export class Articulo {
+  IdArticulo: number;
+  Nombre: string;
+  Precio: number;
+  CodigoDeBarra: string;
+  IdArticuloFamilia: number;
+  Stock: number;
+  FechaAlta: string;
+  Activo: boolean;
+}
